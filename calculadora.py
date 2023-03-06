e8ec83c75c1255c7b99ee18f4c99c04397128215
Iara Aylen Bongiovanni <gio0vanni25@gmail.com>
2023-03-06 14:55:17 -0300

    Primer commit

---
+++ calculadora.py
@@ -0,0 +1,11 @@
+numero1 = 5
+numero2 = 2
+
+def suma(numero1, numero2):
+    total = numero1 + numero2
+    return total
+
+resultado = suma(numero1, numero2)
+
+print('El resultado es: ', resultado)
+
